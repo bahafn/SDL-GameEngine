d5e3b9275df01a41c00a8aedcfc102fa2d2b3836
bahafn <103534448+bahafn@users.noreply.github.com>
2025-02-25 22:49:07 +0200

    Added utils namespace.

--- include/utils/Vector.hpp
+++ include/utils/Vector.hpp
@@ -1,51 +1,53 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-class Vector {
-  //
-  // Static values
-  //
-  static Vector zero_vector;
-  static Vector one_vector;
-  static Vector up_vector;
-  static Vector down_vector;
-  static Vector right_vector;
-  static Vector left_vector;
+namespace utils {
+  class Vector {
+    //
+    // Static values
+    //
+    static Vector zero_vector;
+    static Vector one_vector;
+    static Vector up_vector;
+    static Vector down_vector;
+    static Vector right_vector;
+    static Vector left_vector;
 
-public:
-  //
-  // Variables
-  //
-  float x, y;
+  public:
+    //
+    // Variables
+    //
+    float x, y;
 
-  //
-  // Constructors
-  //
-  Vector();
-  Vector(float x, float y);
+    //
+    // Constructors
+    //
+    Vector();
+    Vector(float x, float y);
 
-  //
-  // Operators
-  //
-  Vector operator+(const Vector& other) const;
-  Vector operator-(const Vector& other) const;
-  Vector operator*(float other) const;
-  Vector operator/(float other) const;
+    //
+    // Operators
+    //
+    Vector operator+(const Vector& other) const;
+    Vector operator-(const Vector& other) const;
+    Vector operator*(float other) const;
+    Vector operator/(float other) const;
 
-  //
-  // Static Methods
-  //
-  static Vector zero();
-  static Vector one();
-  static Vector up();
-  static Vector right();
-  static Vector down();
-  static Vector left();
+    //
+    // Static Methods
+    //
+    static Vector zero();
+    static Vector one();
+    static Vector up();
+    static Vector right();
+    static Vector down();
+    static Vector left();
 
-  //
-  // Methods
-  //
-  float distance(const Vector& other) const;
-};
+    //
+    // Methods
+    //
+    float distance(const Vector& other) const;
+  };
+}
 
 #endif //VECTOR_H
--- src/utils/Vector.cpp
+++ src/utils/Vector.cpp
@@ -1,53 +1,55 @@
 #include "utils/Vector.hpp"
 #include "cmath"
 
-//
-// Static Values
-//
-Vector Vector::zero_vector = Vector();
-Vector Vector::one_vector = Vector(1, 1);
+namespace utils {
+  //
+  // Static Values
+  //
+  Vector Vector::zero_vector = Vector();
+  Vector Vector::one_vector = Vector(1, 1);
 
-Vector Vector::up_vector = Vector(0, 1);
-Vector Vector::down_vector = Vector(0, -1);
-Vector Vector::right_vector = Vector(1, 0);
-Vector Vector::left_vector = Vector(-1, 0);
+  Vector Vector::up_vector = Vector(0, 1);
+  Vector Vector::down_vector = Vector(0, -1);
+  Vector Vector::right_vector = Vector(1, 0);
+  Vector Vector::left_vector = Vector(-1, 0);
 
-//
-// Constructors
-//
-Vector::Vector(): x(0), y(0) {}
+  //
+  // Constructors
+  //
+  Vector::Vector(): x(0), y(0) {}
 
-Vector::Vector(const float x, const float y): x(x), y(y) {}
+  Vector::Vector(const float x, const float y): x(x), y(y) {}
 
-//
-// Operators
-//
-Vector Vector::operator+(const Vector& other) const { return {x + other.x, y + other.y}; }
+  //
+  // Operators
+  //
+  Vector Vector::operator+(const Vector& other) const { return {x + other.x, y + other.y}; }
 
-Vector Vector::operator-(const Vector& other) const { return {x - other.x, y - other.y}; }
+  Vector Vector::operator-(const Vector& other) const { return {x - other.x, y - other.y}; }
 
-Vector Vector::operator*(const float other) const { return {x * other, y * other}; }
+  Vector Vector::operator*(const float other) const { return {x * other, y * other}; }
 
-Vector Vector::operator/(const float other) const { return {x / other, y / other}; }
+  Vector Vector::operator/(const float other) const { return {x / other, y / other}; }
 
-//
-// Methods
-//
-float Vector::distance(const Vector& other) const {
-  return sqrtf(powf(x - other.x, 2) + powf(y - other.y, 2));
+  //
+  // Methods
+  //
+  float Vector::distance(const Vector& other) const {
+    return sqrtf(powf(x - other.x, 2) + powf(y - other.y, 2));
+  }
+
+  //
+  // Static Methods
+  //
+  Vector Vector::zero() { return zero_vector; }
+
+  Vector Vector::one() { return one_vector; }
+
+  Vector Vector::up() { return up_vector; }
+
+  Vector Vector::down() { return down_vector; }
+
+  Vector Vector::right() { return right_vector; }
+
+  Vector Vector::left() { return left_vector; }
 }
-
-//
-// Static Methods
-//
-Vector Vector::zero() { return zero_vector; }
-
-Vector Vector::one() { return one_vector; }
-
-Vector Vector::up() { return up_vector; }
-
-Vector Vector::down() { return down_vector; }
-
-Vector Vector::right() { return right_vector; }
-
-Vector Vector::left() { return left_vector; }
