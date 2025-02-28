dc1de3c9c27f4f9d7587bed996c38dbaa72c0c88
bahafn <103534448+bahafn@users.noreply.github.com>
2025-02-28 20:25:18 +0200

    Added documentation to Renderer class.

--- include/renderer/Renderer.hpp
+++ include/renderer/Renderer.hpp
@@ -6,6 +6,8 @@
 
 class Renderer {
     SDL_Renderer *sdl_renderer;
+
+    /** Saves the last error that occurred in execution. */
     std::string error;
 
 public:
@@ -17,17 +19,26 @@ public:
     //
     // Methods
     //
+    /** @returns true for success or false for fail. Call get_error() for more information. */
     bool render_image(const char *file_path, const SDL_FRect *rect);
+
+    /** @returns true for success or false for fail. Call get_error() for more information. */
     bool render_rect(const SDL_FRect *rect, const SDL_Color *color);
+
+    /** @returns true for success or false for fail. Call get_error() for more information. */
     bool render_line(float x1, float y1, float x2, float y2, const SDL_Color *color);
+
+    /** @returns true for success or false for fail. Call get_error() for more information. */
     bool render_point(float x, float y, const SDL_Color *color);
 
+    /** Updates the screen with any renderer that occurred after the last call.  */
     bool present();
+
     bool clear();
 
     void set_error(const std::string &error);
-    std::string get_error() const;
 
+    std::string get_error() const;
 };
 
 #endif //RENDERER_H
