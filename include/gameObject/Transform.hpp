b3b1154cf071be52fd0b5709f820e569421b1346
bahafn <103534448+bahafn@users.noreply.github.com>
2025-03-02 22:21:52 +0200

    Fixed problems with local position and rotation in Transform class.

--- include/gameObject/Transform.hpp
+++ include/gameObject/Transform.hpp
@@ -25,28 +25,30 @@ public:
 
     Transform(const Vector &position, float rotation, Transform *parent);
 
+    explicit Transform(Transform *parent);
+
     //
     // Methods
     //
     Vector get_position() const;
 
-    Vector get_local_position() const;
-
     float get_rotation() const;
-
-    float get_local_rotation() const;
 
     Transform *get_parent() const;
 
     void set_position(const Vector &position);
 
-    void set_local_position(const Vector &position);
-
     void set_rotation(float rotation);
 
-    void set_local_rotation(float rotation);
-
     void set_parent(Transform *parent);
+
+    Vector get_local_position() const;
+
+    float get_local_rotation() const;
+
+    void set_local_position(const Vector &position);
+
+    void set_local_rotation(float rotation);
 };
 
 #endif //TRANSFORM_H
--- src/gameObject/Transform.cpp
+++ src/gameObject/Transform.cpp
@@ -1,5 +1,10 @@
 #include "gameObject/Transform.hpp"
 
+#include <pstl/execution_defs.h>
+
+//
+// Constructors
+//
 Transform::Transform() = default;
 
 Transform::Transform(const Vector &position, const float rotation): position(position), rotation(rotation) {
@@ -9,54 +14,29 @@ Transform::Transform(const Vector &position, const float rotation, Transform *pa
     rotation(rotation), parent(parent) {
 }
 
+Transform::Transform(Transform *parent): position(parent->position), rotation(parent->rotation), parent(parent) {
+}
 
+//
+// Methods
+//
 Vector Transform::get_position() const { return this->position; }
-
-Vector Transform::get_local_position() const { return this->local_position; }
 
 float Transform::get_rotation() const { return this->rotation; }
 
-float Transform::get_local_rotation() const { return this->local_rotation; }
-
 Transform *Transform::get_parent() const { return this->parent; }
 
-void Transform::set_position(const Vector &position) {
-    this->position = position;
+void Transform::set_position(const Vector &position) { this->position = position; }
 
-    if (parent != nullptr)
-        this->local_position = this->position - parent->get_position();
-    else
-        this->local_position = this->position;
-}
-
-void Transform::set_local_position(const Vector &position) {
-    this->local_position = position;
-
-    if (parent != nullptr)
-        this->position = this->local_position + parent->get_position();
-    else
-        this->position = local_position;
-}
-
-void Transform::set_rotation(const float rotation) {
-    this->rotation = rotation;
-
-    if (parent != nullptr)
-        this->local_rotation = this->rotation - parent->get_rotation();
-    else
-        this->local_rotation = this->rotation;
-}
-
-void Transform::set_local_rotation(const float rotation) {
-    this->local_rotation = rotation;
-
-    if (parent != nullptr)
-        this->rotation = this->local_rotation + parent->get_rotation();
-    else
-        this->rotation = this->local_rotation;
-}
+void Transform::set_rotation(const float rotation) { this->rotation = rotation; }
 
 void Transform::set_parent(Transform *parent) {
+    if (parent == nullptr) {
+        this->local_position = Vector::ZERO_VECTOR;
+        this->parent = nullptr;
+        return;
+    }
+
     const Transform *ancestor = parent;
     while (ancestor != nullptr) {
         if (ancestor == this)
@@ -65,13 +45,33 @@ void Transform::set_parent(Transform *parent) {
         ancestor = ancestor->get_parent();
     }
 
-    if (parent != nullptr) {
-        this->local_position = this->position - parent->get_position();
-        this->local_rotation = this->rotation - parent->get_rotation();
-    } else {
-        this->local_position = this->position;
-        this->local_rotation = this->rotation;
-    }
-
     this->parent = parent;
+}
+
+Vector Transform::get_local_position() const {
+    if (parent == nullptr)
+        return this->position;
+
+    return this->position - parent->position;
+}
+
+float Transform::get_local_rotation() const {
+    if (parent == nullptr)
+        return this->rotation;
+
+    return this->rotation - parent->rotation;
+}
+
+void Transform::set_local_position(const Vector &position) {
+    if (parent != nullptr)
+        this->position = parent->position + position;
+    else
+        this->position = position;
+}
+
+void Transform::set_local_rotation(const float rotation) {
+    if (parent != nullptr)
+        this->rotation = parent->rotation + rotation;
+    else
+        this->rotation = rotation;
 }
